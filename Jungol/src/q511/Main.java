7830f9ad88633eb8ff25207af50354a75012d250
Class01 <Class01@DESKTOP-ILL1MFM>
2023-04-26 17:58:35 +0900

    정올 511번 문제(수정)

---
+++ Jungol/src/q511/Main.java
@@ -0,0 +1,16 @@
+package q511;
+
+public class Main {
+	public static void main(String[] args) {
+//		int a=45;
+//		int b=9;
+//		
+//		System.out.println("55 - 10 = "+a);
+//		System.out.println("2008 - 1999 = "+b);
+		
+		int a = 55, b = 10;
+		System.out.println(a+" - "+b+ " = " + (a-b));
+		a=2008; b=1999;
+		System.out.println(a+" - "+b+" = "+ (a-b));
+	}
+}
