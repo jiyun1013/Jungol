32a86caeb9106b67eab766bc1ec63daa47cbcd6b
Class01 <Class01@DESKTOP-ILL1MFM>
2023-04-27 17:18:02 +0900

    정울 106번 문제

---
+++ Jungol/src/q106/Main.java
@@ -0,0 +1,16 @@
+package q106;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int n1 = 10;
+		int n2 = 20;
+		int n3 = 30;
+		//int n1 = 10, n2 = 20 , n3 =30; 가능
+		
+		System.out.println(n1 + " + " +n2+ " = " +n3);
+		System.out.printf("%d + %d = %d", n1, n2, n3);
+
+	}
+
+}
