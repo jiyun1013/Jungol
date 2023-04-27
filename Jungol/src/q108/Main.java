28c64dcd050dba6fc72e3c2d47d0cd8312b0b0d0
Class01 <Class01@DESKTOP-ILL1MFM>
2023-04-27 18:15:42 +0900

    정올 108번 문제

---
+++ Jungol/src/q108/Main.java
@@ -0,0 +1,11 @@
+package q108;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double d1 = 100.12;
+		int i1 = 50;
+
+		System.out.printf("%.2f * %d = %.0f\n", d1, i1, i1 * d1);
+	}
+}
