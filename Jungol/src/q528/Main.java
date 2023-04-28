dc8cd7d05b453cd437f0e0dba71d847381288b0d
Class01 <Class01@DESKTOP-ILL1MFM>
2023-04-28 13:52:51 +0900

    정올 528번 문제

---
+++ Jungol/src/q528/Main.java
@@ -0,0 +1,19 @@
+package q528;
+
+import java.util.Scanner;  //컨트롤+쉬프트+O
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int number=sc.nextInt();
+		sc.close();
+		
+		if( number >= 0) {
+			System.out.print(number);
+		}else {
+			System.out.print(number+"\nminus");
+		}
+	}
+}
