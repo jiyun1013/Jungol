b8e9173dd18f2c7e558b73ea24342c53c028969c
Class01 <Class01@DESKTOP-ILL1MFM>
2023-05-01 18:13:16 +0900

    정올 528번 문제 리팩토링

---
+++ Jungol/src/q528/refactoring.java
@@ -0,0 +1,18 @@
+package q528;
+
+import java.util.Scanner;  //컨트롤+쉬프트+O
+
+public class refactoring {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int number=sc.nextInt();
+		sc.close();
+		System.out.print(number);
+		
+		if( number >= 0) {
+		}else {
+			System.out.print("\nminus");
+		}
+	}
