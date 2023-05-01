77bdb8b8d14be87ba07552d7307c27eb06044a89
Class01 <Class01@DESKTOP-ILL1MFM>
2023-05-01 18:12:44 +0900

    정올 104번 문제

---
+++ Jungol/src/q104/Main.java
@@ -0,0 +1,12 @@
+package q104;
+
+public class Main {
+	public static void main(String[] args) {
+		int k = 90;
+		int m = 80;
+		int e = 100;
+		
+		System.out.println("kor "+k+"\nmat "+m+"\neng "+e);
+		System.out.println("sum "+( k + m + e));
+	}
+}
