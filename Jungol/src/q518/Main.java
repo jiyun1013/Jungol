c97d7f597979b88f970a15f51f9f3bade7a57c7c
Class01 <Class01@DESKTOP-ILL1MFM>
2023-05-04 15:47:59 +0900

    정올 518번 문제

---
+++ Jungol/src/q518/Main.java
@@ -0,0 +1,17 @@
+package q518;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		int n3 = sc.nextInt();
+		sc.close();
+		int sum = n1 + n2 + n3;
+
+		System.out.printf("sum : " + sum);
+		System.out.printf("\navg : " + (sum / 3));
+	}
+}
