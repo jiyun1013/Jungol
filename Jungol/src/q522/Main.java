4cc48c588eb2afcef8b97bc9b6b25175b93d3b52
Class01 <Class01@DESKTOP-ILL1MFM>
2023-05-10 17:57:06 +0900

    정올 522번 문제

---
+++ Jungol/src/q522/Main.java
@@ -0,0 +1,34 @@
+package q522;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		sc.close();
+
+		if (n1 == n2) {
+			System.out.println("1");
+		} else {
+			System.out.println("0");
+		}
+		
+		if (n1 != n2) {
+			System.out.println("1");
+		} else {
+			System.out.println("0");
+		}
+		
+		
+		System.out.println("------------");
+		
+		
+		int a = (n1==n2) ? 1 : 0;
+		int b = (n1!=n2) ? 1 : 0;
+		
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
