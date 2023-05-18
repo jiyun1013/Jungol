ac546766e78d1c5b594473fc36013d08458eb1b4
Class01 <Class01@DESKTOP-ILL1MFM>
2023-05-18 15:00:15 +0900

    정올 597번 문제

---
+++ Jungol/src/q597/Main.java
@@ -0,0 +1,14 @@
+package q597;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s1 = sc.next();
+		String s2 = sc.next();
+		sc.close();
+
+		System.out.println(s1.length()+s2.length());
+	}
+}
