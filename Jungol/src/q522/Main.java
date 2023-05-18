6819fd8363c3afff09e5a127f820a540b4b0ebd4
Class01 <Class01@DESKTOP-ILL1MFM>
2023-05-18 15:02:51 +0900

    정올 522번 문제

--- Jungol/src/q522/Main.java
+++ Jungol/src/q522/Main.java
@@ -14,21 +14,32 @@ public class Main {
 		} else {
 			System.out.println("0");
 		}
-		
+
 		if (n1 != n2) {
 			System.out.println("1");
 		} else {
 			System.out.println("0");
 		}
-		
-		
+
 		System.out.println("------------");
-		
-		
-		int a = (n1==n2) ? 1 : 0;
-		int b = (n1!=n2) ? 1 : 0;
-		
+
+		int a = (n1 == n2) ? 1 : 0;
+		int b = (n1 != n2) ? 1 : 0;
+
 		System.out.println(a);
 		System.out.println(b);
+
+		System.out.println("------------");
+
+		boolean r = n1 == n2;
+		System.out.println(r);
+		System.out.println(!r);
+
+		System.out.println("------------");
+
+		boolean q = (n1 == n2) ? true : false;
+		String w = (n1 != n2) ? "true" : "false";
+		System.out.println(q);
+		System.out.println(w);
 	}
 }
