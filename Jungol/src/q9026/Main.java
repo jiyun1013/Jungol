b863f7a25b4531d16d194c631c87e98f22a9ce9a
Class01 <Class01@DESKTOP-ILL1MFM>
2023-06-07 15:53:41 +0900

    정올 9026번 문제

---
+++ Jungol/src/q9026/Main.java
@@ -0,0 +1,9 @@
+package q9026;
+
+public class Main {
+	public static void main(String[] args) {
+		int a = 0, b = 1, c = 2;
+
+		System.out.println((a | b) + " " + (b ^ c) + " " + (a & c) + " " + (a ^ b));
+	}
+}
