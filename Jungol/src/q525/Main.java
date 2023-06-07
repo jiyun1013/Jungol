7d6c7bc743d94fd5f4f25517438a900fbc2bc230
Class01 <Class01@DESKTOP-ILL1MFM>
2023-06-07 15:52:23 +0900

    정올 525번 문제

---
+++ Jungol/src/q525/Main.java
@@ -0,0 +1,15 @@
+package q525;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		int n3 = sc.nextInt();
+		sc.close();
+
+		System.out.println((n1 > n2 && n1 > n3) + " " + (n1 == n2 && n1 == n3));
+	}
+}
