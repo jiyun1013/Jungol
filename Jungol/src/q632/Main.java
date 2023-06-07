e4e613be2f00887bed490d0d20e75424fbd958af
Class01 <Class01@DESKTOP-ILL1MFM>
2023-06-07 15:53:19 +0900

    정올 632번 문제

--- Jungol/src/q632/Main.java
+++ Jungol/src/q632/Main.java
@@ -5,13 +5,28 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		//2.
+		int[] inp = new int[3];
+		int max = Integer.MAX_VALUE;
+		int min = 0;
+		for (int i = 0; i < inp.length; i++) {
+			inp[i] = sc.nextInt();
+
+//			if (inp[i] < max) {
+//				min = inp[i];
+//			}
+		}
+		System.out.println(min);
+
+		// 1.
 		int n1 = sc.nextInt();
 		int n2 = sc.nextInt();
 		int n3 = sc.nextInt();
 		sc.close();
 
-		int min = (n1 < n2) ? n1 : n2;
-		int small = (min < n3) ? min : n3;
+		int small = 0;
+		small = (n1 < n2) ? n1 : n2;
+		small = (small < n3) ? small : n3;
 
 		System.out.println(small);
 	}
