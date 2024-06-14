491094cb7304f92090e815f8e0ef02008c1fa10d
jiyun1013 <rkf0645@naver.com>
2024-06-15 01:52:40 +0900

    정올 504번 문제

---
+++ Jungol/src/q504/Main.java
@@ -0,0 +1,9 @@
+package q504;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("(@) (@)");
+		System.out.println("(=^.^=)");
+		System.out.println("(-m-m-)");
+	}
+}
